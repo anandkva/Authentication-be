c84b5182c83cfe1b218a68aefb21c92ab754b14c
Swathysuriyababu12 <swathysuryababu@gmail.com>
2023-09-14 15:31:48 +0530

    redis code added

--- db.js
+++ db.js
@@ -4,11 +4,12 @@ dotenv.config();
 
 
 const connectDb = async () => {
-    try {
-      await mongoose.connect("mongodb+srv://swathy:Swathy1996@cluster0.lvwbu.mongodb.net/", {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      });
+    try{
+    await mongoose.set("strictQuery", false);
+    await mongoose.connect("mongodb+srv://swathy:Swathy1996@cluster0.lvwbu.mongodb.net/", {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
   
       console.log("MongoDB Connected");
     } catch (error) {
