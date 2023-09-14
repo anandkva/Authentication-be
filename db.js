de7cf8ccbc5de7cc7ffec38e91065e40b9ee53b6
Swathysuriyababu12 <swathysuryababu@gmail.com>
2023-09-14 12:24:40 +0530

    changes

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 dotenv.config();
-
+mongoose.set("strictQuery", false);
 const connectDb = async () => {
   try {
     await mongoose.connect(process.env.MongoDb_Url, {
