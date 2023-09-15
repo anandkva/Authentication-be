a6cacd28ee1e9f4e5883838cf85d8b48e01d8f39
Anand Kumar <anand.kva00@gmail.com>
2023-09-15 13:42:16 +0530

    changed

--- redis.js
+++ redis.js
@@ -1,30 +1,33 @@
 const redis = require('redis');
 
 const redisClient = () => {
-    return redis.createClient({
-      host: 'redis://red-ck1umpfhdsdc738g8gp0:6379', // Use IPv4 loopback address
-      port: 6379,
-    });
-  };
- 
-  const client = redisClient();
-  client.on('error', (err) => {
-    console.error('Redis Error:', err);
+  return redis.createClient({
+    host: 'red-ck1umpfhdsdc738g8gp0', // Remove the "redis://" prefix
+    port: 6379,
   });
-  
-  client.on('connect', () => {
-    console.log('Connected to Redis');
-  });
-  client.on('end', () => {
-    console.log('Redis connection ended');
-  })
-  
-  client.on('SIGQUIT', () => {
-    client.quit();
-  })
-  
+};
 
-  
-  //Configure session middleware
-  
-  module.exports=client
+const client = redisClient();
+
+client.on('error', (err) => {
+  console.error('Redis Error:', err);
+});
+
+client.on('connect', () => {
+  console.log('Connected to Redis');
+});
+
+client.on('end', () => {
+  console.log('Redis connection ended');
+});
+
+// Gracefully handle termination signals (e.g., Ctrl+C)
+process.on('SIGINT', () => {
+  client.quit(() => {
+    console.log('Redis connection closed gracefully');
+    process.exit();
+  });
+});
+
+// Export the Redis client
+module.exports = client;
